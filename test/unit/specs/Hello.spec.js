b2b95a7af6dd6539479eb5a1ec5c6a48ee8c9436
Robin Pokorny <me@robinpokorny.com>
2017-07-11 17:13:20 +0200

    :zap: Add snapshots

--- test/unit/specs/Hello.spec.js
+++ test/unit/specs/Hello.spec.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import Hello from '@/components/Hello'
+import { createRenderer } from 'vue-server-renderer'
+
+const renderer = createRenderer();
 
 describe('Hello.vue', () => {
   it('should render correct contents', () => {
@@ -8,4 +11,14 @@ describe('Hello.vue', () => {
     expect(vm.$el.querySelector('.hello h1').textContent)
       .toEqual('Welcome to Your Vue.js App')
   })
+
+  it('renders to a nice snapshot', () => {
+    const vm = new Vue({
+      el: document.createElement('div'),
+      render: h => h(Hello),
+    });
+    renderer.renderToString(vm, (err, str) => {
+      expect(str).toMatchSnapshot();
+    });
+  });
 })
